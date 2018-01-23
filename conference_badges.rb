4d41c331fc8c4243dffe6db26ae9f9b537fdfc70
Kim Gonzales <kimgo589@gmail.com>
2018-01-23 23:46:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(speaker_list)
 end
 
 def printer (attendees)
-  batch_badge_creator(attendees). each do |badge|
+  batch_badge_creator(attendees).each do |badge|
     puts badge
   end
 
